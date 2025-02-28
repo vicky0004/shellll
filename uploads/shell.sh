308ec9f6f60e27b80e9b1ef3725006a1088c260c
Vicky Kumar <111136233+vicky0004@users.noreply.github.com>
2025-02-28 16:26:06 +0530

    Update shell.sh

--- uploads/shell.sh
+++ uploads/shell.sh
@@ -1,3 +1,4 @@
 echo "hello"
 #!/bin/bash
 echo "Script executed on $(date) by vicky kumar" >>  /tmp/exe_log.txt
+dfx canister update-settings --add-controller jtk7l-ssmej-zagdh-p2njf-goos3-ffefd-i47xm-5ypqp-j3zl3-ugghp-lqe dfinance_backend
